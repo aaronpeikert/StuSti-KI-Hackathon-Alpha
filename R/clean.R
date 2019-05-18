4cdf0dcd0ed1f4ed0c4044c40c62ac695f62d831
MD1901 <scanf13@gmail.com>
2019-05-18 11:39:59 +0200

    Add variable mach storing mean of mach-variables

---
+++ R/clean.R
@@ -0,0 +1,19 @@
+if(!require("pacman"))install.packages("pacman")
+pacman::p_load("tidyverse")
+
+#----clean----
+mach %>% select(-matches("\\d[E|I]$")) -> mach
+mach %>% rename(Q3I = Q3A, 
+                Q4I = Q4A,
+                Q6I = Q6A,
+                Q7I = Q7A,
+                Q9I = Q9A,
+                Q10I = Q10A,
+                Q11I = Q11A,
+                Q14I = Q14A,
+                Q16I = Q16A,
+                Q17I = Q17A) -> mach
+
+mach %>% mutate(major = str_to_lower(major), 
+                psy = str_detect(major, "psych") %>% ifelse(is.na(.), FALSE, .)) -> mach
+mach %>% mutate_at(vars(matches("^Q\\d*I$")), ~8 - .) %>% mutate(., mach = rowMeans(select(., starts_with("Q")))) -> mach
